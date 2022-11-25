ef7f9a787b420d52a24a6dcab27e3e231b24d9f0
GriseldaAlonso <gri_alonso@hotmail.com>
2022-11-25 14:21:43 -0500

    template strings de inicio de sesión y registro con estilos básicos

--- src/componentes/login.js
+++ src/componentes/login.js
@@ -1,13 +1,16 @@
-export const juanpatricio = (db, documento) => {
-  const logIn = document.createElement('form');
+export const logIn = (db, documento) => {
+  const formLogIn = document.createElement('form');
   const formulario = `
-        <input id="username" type="text" placeholder="username">
-        <input id="password" type="password" placeholder="password">
-        <button id="btnLogin">Login</button>`;
-  logIn.innerHTML = formulario;
-  const btnLogin = logIn.querySelector('#btnLogin');
-  const username = logIn.querySelector('#username');
-  const password = logIn.querySelector('#password');
+    <h2 class="title">Inicia sesión</h2>
+    <label for="username">Dirección de correo electrónico</label>
+    <input class="input" id="username" type="text" placeholder="ejemplo@ejemplo.com">
+    <label for="password">Contraseña</label>
+    <input class="input" id="password" type="password" placeholder="******">
+    <button class="btnInitial" id="btnLogin">Iniciar Sesión</button>`;
+  formLogIn.innerHTML = formulario;
+  const btnLogin = formLogIn.querySelector('#btnLogin');
+  const username = formLogIn.querySelector('#username');
+  const password = formLogIn.querySelector('#password');
   btnLogin.addEventListener('click', (e) => {
     e.preventDefault();
     db.collection('userLogin').add({
@@ -17,5 +20,5 @@ export const juanpatricio = (db, documento) => {
     username.value = '';
     password.value = '';
   });
-  documento.insertAdjacentElement('beforeend', logIn);
+  documento.insertAdjacentElement('beforeend', formLogIn);
 };
