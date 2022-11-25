aab95b6d97a77d1dbe2e3d1e1adddb0671223872
Gisela Roman Salcedo <gisenoctis@gmail.com>
2022-11-24 19:28:55 -0600

    primer formulario

--- src/main.js
+++ src/main.js
@@ -1,28 +1,52 @@
 import { db } from './lib/firebase.js';
 
 const rootSection = document.querySelector('#root');
-const btnLogin = document.querySelector('#btnLogin');
-const username = document.querySelector('#username');
-const password = document.querySelector('#password');
-const logIn = () => {
-  const formTemplate = `
-  <form action="">
-  <input id="username" type="text" placeholder="Username">
-  <input id="password" type="password" placeholder="Password">
-  <button id="btnLogin" type="button">Login</button>
-  </form>
-`;
-  rootSection.innerHTML += formTemplate;
-};
+// const btnLogin = document.querySelector('#btnLogin');
+// const username = document.querySelector('#username');
+// const password = document.querySelector('#password');
+// const logIn = () => {
+//   const formTemplate = `
+//   <form>
+//   <input id="username" type="text" placeholder="Username">
+//   <input id="password" type="password" placeholder="Password">
+//   <button id="btnLogin" type="submit">Login</button>
+//   </form>
+// `;
+//   rootSection.innerHTML += formTemplate;
+// };
 
-window.onload = logIn();
+// window.onload = logIn();
 
-const userData = () => {
-  db.collection('userLogin').add({
-    username: username.value,
-    bio: password.value,
+// btnLogin.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   db.collection('userLogin').add({
+//     username: username.value,
+//     bio: password.value,
+//   });
+//   username.value = '';
+//   password.value = '';
+// });
+const juanpatricio = () => {
+  const logIn = document.createElement('form');
+  const formulario = `
+      <input id="username" type="text" placeholder="username">
+      <input id="password" type="password" placeholder="password">
+      <button id="btnLogin">Login</button>`;
+  logIn.innerHTML = formulario;
+  const btnLogin = logIn.querySelector('#btnLogin');
+  const username = logIn.querySelector('#username');
+  const password = logIn.querySelector('#password');
+  btnLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    db.collection('userLogin').add({
+      username: username.value,
+      bio: password.value,
+    });
+    username.value = '';
+    password.value = '';
   });
-  username.value = '';
-  password.value = '';
+  rootSection.insertAdjacentElement('beforeend', logIn);
+
 };
-btnLogin.addEventListener('click', userData());
+
+window.onload = juanpatricio;
