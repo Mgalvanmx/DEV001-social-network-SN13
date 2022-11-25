c8b1eb0b5cab5300fc3db0f262f3d5e34b541e63
Gisela Roman Salcedo <gisenoctis@gmail.com>
2022-11-25 09:28:02 -0600

    acomodando archivo login

--- src/form.js
+++
@@ -1,9 +0,0 @@
-const rootSection = document.getElementById('root');
-const logIn = `
-    <form action="">
-        <input type="text" placeholder="Username">
-        <input type="password" placeholder="Password">
-        <button id="btnLogin">Login</button>
-    </form>
-`;
-rootSection.appendChild(logIn);
