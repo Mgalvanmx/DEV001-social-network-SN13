10a262d6e13c3a43e2f620424649286b21d3a991
GriseldaAlonso <gri_alonso@hotmail.com>
2022-11-28 10:03:27 -0500

    solución al error de imports en firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCkD69li7OY4KM6LvGVOcBHZjg8wPRTngY',
   authDomain: 'rrss-peliculas.firebaseapp.com',
   projectId: 'rrss-peliculas',
-  storageBucket: 'rss-peliculas.appspot.com',
+  storageBucket: 'rrss-peliculas.appspot.com',
   messagingSenderId: '700965177786',
   appId: '1:700965177786:web:f577aa4d2e9f4ba5a5f583',
   measurementId: 'G-VBM6GMN9EZ',
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-export const db = firebase.firestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export const db = getFirestore(app);
